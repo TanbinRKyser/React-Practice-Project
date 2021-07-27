46f562e8310753f7692fc0c302bcaeb1bcd89f2d
foxcatcher24 <tmitul515@gmail.com>
2021-07-27 19:38:05 +0600

    270721.8.16 - adding build control

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./BuildControls.module.css";
+import BuildControl from "../BuildControls/BuildControl/BuildControl";
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+];
+
+
+const buildControls = ( props ) => (
+    <div className={styles.BurgerControls}>
+        {controls.map( ctrl => {
+            return <BuildControl key={ctrl.label} label={ctrl.label} />
+        })}
+    </div>
+);
+
+export default buildControls;
