aad48b6894fd94df6811415a0a3f870b6f54034b
foxcatcher24 <tmitul515@gmail.com>
2021-09-08 17:38:28 +0600

    070921.12.10 - Implementing navigation links

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,10 @@
 import styles from './Order.module.css';
 
-
-const order = (props) => {
+const order = (props) => (
     <div className={styles.Order}>
         <p>Ingredients: Salad(1)</p>
         <p>Price: <strong>USD 4.99</strong></p>
     </div>
-}
+);
 
 export default order;
