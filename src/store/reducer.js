f0e2479f727c6c9fea5ece2d6174382981c8aa4c
foxcatcher24 <tmitul515@gmail.com>
2021-09-13 14:14:00 +0600

    130921.15.3 -- basic redux setup

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    ingredients : null,
+    totalPrice: 2.99,
+}
+
+const reducer = ( state=initialState, action ) => {
+    
+};
+
+export default reducer;
