7b055899a26305dcdbb5c3bede19c0a2ad252a33
foxcatcher24 <tmitul515@gmail.com>
2021-09-18 17:20:26 +0600

    180921.18.10 -- logging out after a time

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,6 +24,20 @@ export const authFail = ( error ) => {
     }
 }
 
+export const logout = () => {
+    return{
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const  checkAuthTimeout = ( expirationTime ) => {
+    return dispatch => {
+        setTimeout( () => {
+            dispatch( logout() );
+        }, expirationTime * 1000 );
+    }
+}
+
 export const auth = ( email, password, isSignup ) => {
     return dispatch => {
         dispatch( authStart() );
@@ -44,8 +58,9 @@ export const auth = ( email, password, isSignup ) => {
         // console.log( url );
         axios.post( url, authData )
             .then( response => {
-                console.log( response );
+                // console.log( response );
                 dispatch( authSuccess( response.data.idToken, response.data.localId ) );
+                dispatch( checkAuthTimeout( response.data.expiresIn ) );
             })
             .catch( error => {
                 dispatch( authFail( error.response.data.error ) );
