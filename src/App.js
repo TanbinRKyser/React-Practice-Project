c24e7983c94969ddb0e8c18bb930264b7593a99b
foxcatcher24 <tmitul515@gmail.com>
2021-09-20 13:10:20 +0600

    200921.19.7 -- using environment variables and lazy loading

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+import asyncComponent from './hoc/asyncComponent/asyncCompoent';
+
+const asyncCheckout = asyncComponent( () => {
+  return import('./containers/Checkout/Checkout')
+});
+
+const asyncOrders = asyncComponent( () => {
+  return import('./containers/Orders/Orders')
+});
+
+const asyncAuth = asyncComponent( () => {
+  return import('./containers/Auth/Auth')
+});
+
+
 
 class App extends Component{
 
@@ -19,17 +31,17 @@ class App extends Component{
   render(){
 
     let protectedRoutes = (<Switch>
-                              <Route path="/auth" component={Auth}/> 
-                              <Route path="/" component={BurgerBuilder} exact/> 
+                              <Route path="/auth" component={ asyncAuth }/> 
+                              <Route path="/" component={ BurgerBuilder } exact/> 
                               <Redirect to="/" />
                           </Switch> );
     if( this.props.authenticated ){
       protectedRoutes = ( <Switch>
-                            <Route path="/checkout" component={Checkout}/> 
-                            <Route path="/orders" component={Orders}/> 
-                            <Route path="/auth" component={Auth}/> 
-                            <Route path="/logout" component={Logout}/>
-                            <Route path="/" component={BurgerBuilder} exact/>  
+                            <Route path="/checkout" component={ asyncCheckout }/> 
+                            <Route path="/orders" component={ asyncOrders }/> 
+                            <Route path="/auth" component={ asyncAuth }/> 
+                            <Route path="/logout" component={ Logout }/>
+                            <Route path="/" component={ BurgerBuilder } exact/>  
                             <Redirect to="/" />
                           </Switch> );
     }
